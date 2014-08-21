49b3ae9e4c3f440e0bb11504495aa692258a7403
clebert suconic <clebert.suconic@gmail.com>
2014-08-21 15:29:49 -0400

    RequiredParameterForAnnotationCheck

--- sevntu-checks/src/main/java/com/github/sevntu/checkstyle/checks/annotation/ForbidAnnotationCheck.java
+++ sevntu-checks/src/main/java/com/github/sevntu/checkstyle/checks/annotation/ForbidAnnotationCheck.java
@@ -38,7 +38,7 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  * @author <a href="mailto:hidoyatov.v.i@gmail.com">Hidoyatov Victor</a>
  */
 
-public class ForbidAnnotationCheck extends Check
+public class ForbidAnnotationCheck extends AnnotationAbstract
 {
 	
 	/**
@@ -108,18 +108,7 @@ public class ForbidAnnotationCheck extends Check
                     currentTarget, annotationName);
         }
     }
-    
-    private static String getAnnotationName(DetailAST aAnnotation){
-    	DetailAST directname = aAnnotation.findFirstToken(TokenTypes.IDENT);
 
-    	if(directname != null){
-    		return directname.getText();
-    	}else{
-    		//This means that annotation is specified with the full package name
-    		return aAnnotation.findFirstToken(TokenTypes.DOT).getLastChild().getText();
-    	}
-    }
-    
 
     /**
      * return true if mAnnotationNames contains aAnnotationName.
